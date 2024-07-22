c0e3a311c54e48a4d5672c8b029356627250b043
mjedryczka <jedryczkamaciej@gmail.com>
2024-07-22 15:16:13 -0400

    Found last error in delete endpoint

--- app/routers/qr_code.py
+++ app/routers/qr_code.py
@@ -66,7 +66,7 @@ async def list_qr_codes_endpoint(token: str = Depends(oauth2_scheme)):
     ) for qr_file in qr_files]
     return responses
 
-@router.delete("/qr-codes/{qr_fileame}", status_code=status.HTTP_200_OK, tags=["QR Codes"])
+@router.delete("/qr-codes/{qr_filename}", status_code=status.HTTP_204_NO_CONTENT, tags=["QR Codes"])
 async def delete_qr_code_endpoint(qr_filename: str, token: str = Depends(oauth2_scheme)):
     logging.info(f"Deleting QR code: {qr_filename}.")
     qr_code_path = QR_DIRECTORY / qr_filename
@@ -75,5 +75,4 @@ async def delete_qr_code_endpoint(qr_filename: str, token: str = Depends(oauth2_
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="QR code not found")
 
     delete_qr_code(qr_code_path)
-    # No need to generate or return any links since the 204 response should not contain a body
     return Response(status_code=status.HTTP_204_NO_CONTENT)
